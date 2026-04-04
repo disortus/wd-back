ef8b540c8d5f8970b6f07bfb8232c8c848f3623a
disortus <disortus.voidcaster@proton.me>
2026-04-05 01:59:17 +0500

    cors fix

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 
 // CORS
 app.use(cors({
-    origin: ["http://localhost:5173", "https://wd-front-41qu.vercel.app"],
+    origin: ["http://localhost:5173", "https://wd-front-41qu.vercel.app", "https://wd-front.vercel.app"],
     credentials: true
 }));
 
